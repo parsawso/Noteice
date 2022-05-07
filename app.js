6d429a30452420b10145afd9ab57a63b9ad973ea
parsaw <parsawso@gmail.com>
2022-05-08 00:12:10 +0430

    make sidebar-header smoother

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const sidebar = document.getElementById('sidebar');
+const sidebarHeader = document.getElementById('sidebar-header');
+
 sidebar.style.width = '0';
+sidebarHeader.style.opacity = '0';
 
 function openSidebar() {
     sidebar.style.width = '90%';
+    sidebarHeader.style.opacity = '1';
 }
 
 function closeSidebar() {
+    sidebarHeader.style.opacity = '0';
     sidebar.style.width = '0';
 }
