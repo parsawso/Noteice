0088a5e51f257a9da160ab8eeec3d419cfca5427
parsaw <parsawso@gmail.com>
2022-05-12 13:26:44 +0430

    top-bar scroll down and up animation

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ function closeSidebar() {
     sidebarHeader.style.opacity = '0';
     sidebar.style.width = '0';
 }
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("top-bar").style.top = "1rem";
+  } else {
+    document.getElementById("top-bar").style.top = "-6rem";
+  }
+  prevScrollpos = currentScrollPos;
+}
