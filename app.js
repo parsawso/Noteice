b4be652088e7400fde803e0dc2fd828cedaf7785
parsaw <parsawso@gmail.com>
2022-05-15 19:27:46 +0430

    freeze scrolling when circle button or sidebar button is pushed

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+const mainBody = document.querySelector("#main-body");
+const sidebarButton = document.querySelector(".burger-menu-icon");
 const sidebar = document.getElementById("sidebar");
+const addNotesSection = document.getElementById("add-notes-section");
 
+// sidebar action
 sidebar.style.left = "-40rem";
 sidebar.style.boxShadow = "none";
 
 function openSidebar() {
   sidebar.style.left = "0";
   sidebar.style.boxShadow = "0 0 100rem black";
+  mainBody.style.height = "100%";
+  mainBody.style.overflow = "hidden";
 }
 
 function closeSidebar() {
   sidebar.style.left = "-40rem";
   sidebar.style.boxShadow = "none";
+  mainBody.style.height = "auto";
+  mainBody.style.overflow = "visible";
 }
 
+//top bar action
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
@@ -25,8 +34,8 @@ window.onscroll = function () {
 };
 
 // Open and close add notes
-const addNotesSection = document.getElementById("add-notes-section");
-
 function openAddNotes() {
   addNotesSection.style.top = "2rem";
+  mainBody.style.height = "100%";
+  mainBody.style.overflow = "hidden";
 }
