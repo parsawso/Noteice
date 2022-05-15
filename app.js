794c212e0428be79763959a2de1a2a2eeabfe560
parsaw <parsawso@gmail.com>
2022-05-15 19:02:29 +0430

    I don't know why but all \'s have turned into "s :|

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementById("sidebar");
 
-sidebar.style.left = '-40rem';
-sidebar.style.boxShadow = 'none';
+sidebar.style.left = "-40rem";
+sidebar.style.boxShadow = "none";
 
 function openSidebar() {
-    sidebar.style.left = '0';
-    sidebar.style.boxShadow = '0 0 100rem black';
+  sidebar.style.left = "0";
+  sidebar.style.boxShadow = "0 0 100rem black";
 }
 
 function closeSidebar() {
-    sidebar.style.left = '-40rem';
-    sidebar.style.boxShadow = 'none';
+  sidebar.style.left = "-40rem";
+  sidebar.style.boxShadow = "none";
 }
 
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("top-bar").style.top = "1rem";
@@ -22,11 +22,11 @@ window.onscroll = function() {
     document.getElementById("top-bar").style.top = "-6rem";
   }
   prevScrollpos = currentScrollPos;
-}
+};
 
 // Open and close add notes
-const addNotesSection = document.getElementById('add-notes-section');
+const addNotesSection = document.getElementById("add-notes-section");
 
 function openAddNotes() {
-    addNotesSection.style.top = '2rem';
+  addNotesSection.style.top = "2rem";
 }
