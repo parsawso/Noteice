986e843afeae4047578b47f4a673836c3613d23e
parsaw <parsawso@gmail.com>
2022-06-04 19:50:45 +0430

    create adding note event

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const sidebarButton = document.querySelector(".burger-menu-icon");
 const sidebar = document.querySelector("#sidebar");
 const addNotesSection = document.querySelector("#add-notes-section");
 const addNoteButton = document.querySelector("#add-note-button");
+const submitNoteBTN = document.querySelector(".submit-note-btn");
+const cardsSection = document.querySelector(".cards-section");
 
 // Default Values
 mainBody.style.filter = "brightness(1)";
@@ -55,3 +57,29 @@ function closeAddNotes() {
   mainBody.style.filter = "brightness(1)";
   addNoteButton.style.bottom = "3rem";
 }
+
+// collecting data from adding note form
+submitNoteBTN.addEventListener("click",() => {
+  if (document.querySelector(".header-field").value==="" || document.querySelector(".body-field").value==="") {
+    alert("Please fill all the fields")
+  }
+  
+  const noteHeader = document.querySelector(".header-field").value;
+  document.querySelector(".header-field").value = null;
+  
+  const noteBody = document.querySelector(".body-field").value;
+  document.querySelector(".body-field").value = null;
+
+  cardsSection.innerHTML += (`<div class="card">
+  <section class="card-header">${noteHeader}</section>
+  <section class="card-body">${noteBody}
+  </section>
+  <section class="card-footer">
+    <i class="fa-solid fa-trash fa-2x delete-icon"></i>
+    <i class="fa-solid fa-pen fa-2x edit-icon"></i>
+    <section class="card-category">Category</section>
+  </section>
+</div>`);
+
+  closeAddNotes();
+})
